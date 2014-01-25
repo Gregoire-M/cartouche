52a1cff495b6c9cf7eb87e57b81a267bdff15549
Grégoire Marchal <gregoire.marchal@gmail.com>
2014-01-25 18:31:38 +0100

    Cartouche bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Cartouche\FrontBundle\CartoucheFrontBundle(),
+            new Cartouche\CartoucheBundle\CartoucheCartoucheBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Cartouche/CartoucheBundle/CartoucheCartoucheBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Cartouche\CartoucheBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CartoucheCartoucheBundle extends Bundle
+{
+}
