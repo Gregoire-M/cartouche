15efd8b02fbb8eedc7ae2013dbf3caaf28712080
Grégoire Marchal <gmarchal@efidev.com>
2014-01-27 12:02:03 +0100

    Missing NS

--- src/Cartouche/FrontBundle/Controller/DefaultController.php
+++ src/Cartouche/FrontBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace Cartouche\FrontBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
